1bef11095ca0511463a3249937f81ccdfe5415a4
Viskup, Ondřej <viskuond@fel.cvut.cz>
2020-09-28 15:16:30 +0200

    Logged in basic layout

--- src/controllers/workout.js
+++ src/controllers/workout.js
@@ -1,3 +1,3 @@
 module.exports.showWorkouts  = function(req,res,next){
-    res.render('workouts', {title: "Workouts"});
+    res.render('workouts', {title: "Workouts", username: req.user.email});
 }
