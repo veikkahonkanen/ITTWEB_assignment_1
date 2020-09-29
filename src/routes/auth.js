a96f6bc90472761cf477c17bccb7417f7370a16a
David <dmarquez.estudis@gmail.com>
2020-09-29 13:12:53 +0200

    fix logout error

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,17 +4,16 @@ const ensureLogin= require("connect-ensure-login");
 
 var router = express.Router();
 
+router.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
 
 router.use(ensureLogin.ensureLoggedOut("/workouts"));
 router.get("/login", auth.showLogin);
 router.get("/register", auth.showRegister);
 
 router.post('/login', auth.loginUser);
-
-router.get("/logout", function (req, res) {
-  req.logout();
-  res.redirect("/");
-})
 
 router.post("/register", auth.registerUser);
 module.exports = router;
